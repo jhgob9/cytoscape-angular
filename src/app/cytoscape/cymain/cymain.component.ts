83e1affdc6264173ad5589edfae489f94d28a0d7
고재훈 <jhgo@bitnine.net>
2019-03-28 23:46:30 +0900

    로그아웃 버튼 추가

--- src/app/cytoscape/cymain/cymain.component.ts
+++ src/app/cytoscape/cymain/cymain.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 import { SubtitleService } from 'src/app/services/subtitle.service';
+import { AppState, selectAuthState } from './../../store/app.states';
+import { LogOut } from './../../store/actions/auth.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cymain',
@@ -11,16 +15,26 @@ import { SubtitleService } from 'src/app/services/subtitle.service';
 })
 
 export class CymainComponent implements OnInit {
+  getState: Observable<any>;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
     map(result => result.matches)
   );
   constructor(
+    private store: Store<AppState>,
     public SubTitle: SubtitleService,
-    private breakpointObserver: BreakpointObserver
-  ) { }
+    private breakpointObserver: BreakpointObserver,
+    public router: Router
+  ) {
+    this.getState = this.store.select(selectAuthState);
+  }
 
   ngOnInit() {
     this.SubTitle.subtitle = 'Cytoscape.js Demos';
+  }
+
+  logOut(): void {
+    this.store.dispatch(new LogOut());
+    this.router.navigate(['/login']);
   }
 }
