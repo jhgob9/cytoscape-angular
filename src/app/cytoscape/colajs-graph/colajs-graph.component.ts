ad746cf446b55bdc73f76b8bb8f85cea44a1c4bc
고재훈 <jhgo@bitnine.net>
2019-04-02 16:59:10 +0900

    Cola.js gene-gene graph 스타일 변경 작업

--- src/app/cytoscape/colajs-graph/colajs-graph.component.ts
+++ src/app/cytoscape/colajs-graph/colajs-graph.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { forkJoin } from 'rxjs';
+import { forkJoin, fromEvent } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
 
 // import * as cytoscape from 'cytoscape';
 // import * as _ from 'lodash';
@@ -27,31 +29,34 @@ export class ColajsGraphComponent implements OnInit {
     this.SubTitle.subtitle = 'Cola.js gene-gene graph';
     const dataGet = this.http.get('./assets/colajs-graph/data.json');
     const styleGet = this.http.get('./assets/colajs-graph/cy-style.json');
+    const click$ = fromEvent(document, 'click').pipe(
+      pluck('target')
+    );
     const data$ = forkJoin(dataGet, styleGet);
     data$.subscribe((value: any) => {
-      let h = function (tag, attrs, children) {
+      const h = (tag: any, attrs: any, children: any) => {
         const el = document.createElement(tag);
 
-        Object.keys(attrs).forEach(function (key) {
+        Object.keys(attrs).forEach((key: any) => {
           const val = attrs[key];
 
           el.setAttribute(key, val);
         });
 
-        children.forEach(function (child) {
+        children.forEach((child: any) => {
           el.appendChild(child);
         });
 
         return el;
       };
 
-      const t = function (text) {
+      const t = (text: any) => {
         const el = document.createTextNode(text);
 
         return el;
       };
 
-      const $ = document.querySelector.bind(document);
+      // const $ = document.querySelector.bind(document);
 
       let cy = this.cy;
       cy = cytoscape({
@@ -77,7 +82,7 @@ export class ColajsGraphComponent implements OnInit {
         class: 'param'
       }, []);
 
-      var $config = $('#config');
+      const $config = $('#config')[0];
       $config.appendChild($btnParam);
 
       const sliders = [
@@ -98,24 +103,18 @@ export class ColajsGraphComponent implements OnInit {
 
       const buttons = [
         {
-          label: h('span', { class: 'fa fa-random' }, []),
+          // label: h('mat-icon', { svgIcon: 'arrow_downward' }, []),
+          label: h('span', {}, [t('Random')]),
           layoutOpts: {
             randomize: true,
             flow: null
-          }
-        },
-
-        {
-          label: h('span', { class: 'fa fa-long-arrow-down' }, []),
-          layoutOpts: {
-            flow: { axis: 'y', minSeparation: 30 }
           }
         }
       ];
 
       sliders.forEach(makeSlider);
 
-      buttons.forEach(makeButton);
+      // buttons.forEach(makeButton);
 
       function makeLayout(opts?: any) {
         params.randomize = false;
@@ -128,7 +127,7 @@ export class ColajsGraphComponent implements OnInit {
         return cy.layout(params);
       }
 
-      function makeSlider(opts) {
+      function makeSlider(opts?: any) {
         const $input = h('input', {
           id: 'slider-' + opts.param,
           type: 'range',
@@ -146,8 +145,8 @@ export class ColajsGraphComponent implements OnInit {
         $param.appendChild($label);
         $param.appendChild($input);
         $config.appendChild($param);
-        
-        const update = _.throttle(function () {
+
+        const update = _.throttle(() => {
           params[opts.param] = $input.value;
           layout.stop();
           layout = makeLayout();
@@ -158,16 +157,14 @@ export class ColajsGraphComponent implements OnInit {
         $input.addEventListener('change', update);
       }
 
-      function makeButton(opts) {
-        const $button = h('button', { class: 'btn btn-default' }, [opts.label]);
-
+      function makeButton(opts?: any) {
+        const $button = h('button', { 'mat-icon-button': '' }, [opts.label]);
         $btnParam.appendChild($button);
 
-        $button.addEventListener('click', function () {
+        $button.addEventListener('click', () => {
           layout.stop();
 
           if (opts.fn) { opts.fn(); }
-
           layout = makeLayout(opts.layoutOpts);
           layout.run();
         });
@@ -185,7 +182,7 @@ export class ColajsGraphComponent implements OnInit {
         return ti;
       }
 
-      const hideTippy = function (node) {
+      const hideTippy = (node: any) => {
         const tippy = node.data('tippy');
 
         if (tippy != null) {
@@ -193,24 +190,24 @@ export class ColajsGraphComponent implements OnInit {
         }
       };
 
-      const hideAllTippies = function () {
+      const hideAllTippies = () => {
         cy.nodes().forEach(hideTippy);
       };
 
-      cy.on('tap', function (e) {
+      cy.on('tap', (e: any) => {
         if (e.target === cy) {
           hideAllTippies();
         }
       });
 
-      cy.on('tap', 'edge', function (e) {
+      cy.on('tap', 'edge', (e: any) => {
         hideAllTippies();
       });
 
-      cy.on('zoom pan', function (e) {
+      cy.on('zoom pan', (e: any) => {
         hideAllTippies();
       });
-      cy.nodes().forEach(function (n: any) {
+      cy.nodes().forEach((n: any) => {
         const g = n.data('name');
         const $links = [
           {
@@ -225,7 +222,7 @@ export class ColajsGraphComponent implements OnInit {
             name: 'GeneMANIA',
             url: 'http://genemania.org/search/human/' + g
           }
-        ].map(function (link) {
+        ].map((link: any) => {
           return h('a', { target: '_blank', href: link.url, class: 'tip-link' }, [t(link.name)]);
         });
 
@@ -233,10 +230,63 @@ export class ColajsGraphComponent implements OnInit {
 
         n.data('tippy', tippy);
 
-        n.on('click', function (e) {
+        n.on('click', (e: any) => {
           tippy.show();
           cy.nodes().not(n).forEach(hideTippy);
         });
+      });
+      click$.subscribe((tar) => {
+        const $btn = $(tar).closest('button');
+        if ($btn.hasClass('btn_cached')) { // 레이아웃 랜덤 버튼
+          layout.stop();
+          layout = makeLayout({
+            randomize: true,
+            flow: null
+          });
+          layout.run();
+        }
+        if ($btn.hasClass('btn_node_color_red')) { // nodes 컬러 레드 변경
+          /* cy.nodes().css({ // 리셋이 되지 않음, 중간에 임의로 바꾸는 스타일이 아닌 초반에 스타일을 결정할 때 사용하는 것이 좋을 듯
+            backgroundColor: 'red'
+          }); */
+          cy.style().selector('node')
+            .style('background-color', 'red')
+            .update();
+        }
+        if ($btn.hasClass('btn_edge_color_red')) { // edges 컬러 레드 변경
+          cy.style().selector('edge')
+            .style('line-color', 'red')
+            .update();
+        }
+        /* if ($btn.hasClass('btn_edge_color_random')) { // edges 컬러 랜덤 변경
+          cy.style().selector('edge')
+            .style({
+              'line-color': 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255)+')'
+            })
+            .update();
+          // Math.floor(Math.random() * 255)
+        } */
+        if ($btn.hasClass('btn_font_color_red')) { // font 컬러 레드 변경
+          cy.style().selector('node')
+            .style('color', 'red')
+            .update();
+        }
+        if ($btn.hasClass('btn_reset')) { // 컬러 리셋
+          cy.style(value[1]).update();
+        }
+
+
+      /* cy.elements().forEach((el: any) => {
+        el.addClass('node_red');
+        console.log(el);
+      }); */
+      /* cy.elements().forEach((el: any) => {
+        console.log(el.id());
+      }); */
+          // console.log( cy.$('[group = "nodes"]') );
+      /* cy.nodes().forEach((node: any) => {
+        console.log(node.parent());
+      }); */
       });
     });
   }
--- src/app/cytoscape/tokyo-railways/tokyo-railways.component.ts
+++ src/app/cytoscape/tokyo-railways/tokyo-railways.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import * as cytoscape from 'cytoscape';
+// import * as cytoscape from 'cytoscape';
 
 import { SubtitleService } from 'src/app/services/subtitle.service';
 
@@ -12,7 +12,6 @@ import { SubtitleService } from 'src/app/services/subtitle.service';
 })
 export class TokyoRailwaysComponent implements OnInit {
   public cy: any;
-  public cySet = [];
 
   constructor(
     public SubTitle: SubtitleService,
@@ -22,6 +21,32 @@ export class TokyoRailwaysComponent implements OnInit {
   ngOnInit() {
     this.SubTitle.subtitle = 'Tokyo railways';
     let cy = this.cy;
+
+
+    // hyperscript-like function
+    var h = function (tag, attrs, children) {
+      var el = document.createElement(tag);
+
+      if (attrs != null && typeof attrs === typeof {}) {
+        Object.keys(attrs).forEach(function (key) {
+          var val = attrs[key];
+
+          el.setAttribute(key, val);
+        });
+      } else if (typeof attrs === typeof []) {
+        children = attrs;
+      }
+
+      if (children != null && typeof children === typeof []) {
+        children.forEach(function (child) {
+          el.appendChild(child);
+        });
+      } else if (children != null && typeof children === typeof '') {
+        el.appendChild(document.createTextNode(children));
+      }
+
+      return el;
+    };
     var toJson = function (obj) { return obj.json(); };
     var toText = function (obj) { return obj.text(); };
 
@@ -50,8 +75,189 @@ export class TokyoRailwaysComponent implements OnInit {
         boxSelectionEnabled: false
       });
 
-      /* mendData();
-      bindRouters(); */
+      mendData();
+      bindRouters();
+    }
+    function mendData() {
+      // because the source data doesn't connect nodes properly, use the cytoscape api to mend it:
+
+      cy.startBatch();
+
+      // put nodes in bins based on name
+      var nodes = cy.nodes();
+      var bin = {};
+      var metanames = [];
+      for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        var name = node.data('station_name');
+        var nbin = bin[name] = bin[name] || [];
+
+        nbin.push(node);
+
+        if (nbin.length === 2) {
+          metanames.push(name);
+        }
+      }
+
+      // connect all nodes together with walking edges
+      for (var i = 0; i < metanames.length; i++) {
+        var name = metanames[i];
+        var nbin = bin[name];
+
+        for (var j = 0; j < nbin.length; j++) {
+          for (var k = j + 1; k < nbin.length; k++) {
+            var nj = nbin[j];
+            var nk = nbin[k];
+
+            cy.add({
+              group: 'edges',
+              data: {
+                source: nj.id(),
+                target: nk.id(),
+                is_walking: true
+              }
+            });
+
+            //.css({
+            //    'line-color': 'yellow'
+            // });
+          }
+        }
+
+      }
+
+      cy.endBatch(); //.autolock( true );
+    }
+
+    var start, end;
+    var $body = document.body;
+
+    function selectStart(node) {
+      clear();
+
+      $body.classList.add('has-start');
+
+      start = node;
+
+      start.addClass('start');
+    }
+
+    function selectEnd(node) {
+      $body.classList.add('has-end', 'calc');
+
+      end = node;
+
+      cy.startBatch();
+
+      end.addClass('end');
+
+      setTimeout(function () {
+        var aStar = cy.elements().aStar({
+          root: start,
+          goal: end,
+          weight: function (e) {
+            if (e.data('is_walking')) {
+              return 0.25; // assume very little time to walk inside stn
+            }
+
+            return e.data('is_bullet') ? 1 : 3; // assume bullet is ~3x faster
+          }
+        });
+
+        if (!aStar.found) {
+          $body.classList.remove('calc');
+          clear();
+
+          cy.endBatch();
+          return;
+        }
+
+        cy.elements().not(aStar.path).addClass('not-path');
+        aStar.path.addClass('path');
+
+        cy.endBatch();
+
+        $body.classList.remove('calc');
+      }, 300);
+    }
+
+    function clear() {
+      $body.classList.remove('has-start', 'has-end');
+      cy.elements().removeClass('path not-path start end');
+    }
+
+    var shownTippy;
+
+    function makeTippy(node, html) {
+      removeTippy();
+
+      shownTippy = tippy(node.popperRef(), {
+        html: html,
+        trigger: 'manual',
+        arrow: true,
+        placement: 'bottom',
+        hideOnClick: false,
+        duration: [250, 0],
+        theme: 'light',
+        interactive: true,
+        onHidden: function (tip) {
+          if (tip != null) {
+            tip.destroy();
+          }
+        }
+      }).tooltips[0];
+
+      shownTippy.show();
+
+      return shownTippy;
+    }
+
+    function removeTippy() {
+      if (shownTippy) {
+        shownTippy.hide();
+      }
+    }
+
+    function bindRouters() {
+
+      var $clear = $('#clear');
+
+      cy.on('tap pan zoom', function (e) {
+        if (e.target === cy) {
+          removeTippy();
+        }
+      });
+
+      cy.on('tap', 'node', function (e) {
+        var node = e.target;
+
+        var start = h('button', { id: 'start' }, 'START');
+
+        start.addEventListener('click', function () {
+          var n = cy.$('node:selected');
+
+          selectStart(n);
+
+          removeTippy();
+        });
+
+        var end = h('button', { id: 'end', }, 'END');
+
+        end.addEventListener('click', function () {
+          var n = cy.$('node:selected');
+
+          selectEnd(n);
+
+          removeTippy();
+        });
+
+        var html = h('div', { className: 'select-buttons' }, [start, end]);
+
+        makeTippy(node, html);
+      });
+
+      // $clear.addEventListener('click', clear);
+      $clear.on('click', clear);
     }
    /*  this.http.get('./assets/tokyo-railways/tokyo-railways.json').subscribe(data => {
       this.cySet.push(data);
@@ -61,7 +267,4 @@ export class TokyoRailwaysComponent implements OnInit {
       });
     }); */
   }
-}
-function CySet(obj: any) {
-  console.log(obj.cySet);
 }
